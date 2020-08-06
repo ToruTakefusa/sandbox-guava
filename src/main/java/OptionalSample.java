f54e22a100e7beb08be0204401e2a4381958301c
Toru Takefusa <torutakefusa@gmail.com>
2020-08-06 18:51:17 +0900

    Add practice of Optional.absent()

--- src/main/java/OptionalSample.java
+++ src/main/java/OptionalSample.java
@@ -3,10 +3,11 @@ import com.google.common.base.Optional;
 public class OptionalSample {
     public static void main(String[] args) {
         OptionalSample optionalSample = new OptionalSample();
-        optionalSample.practiceOf();
+        optionalSample.practiceOptionalOf();
+        optionalSample.practiceAbsent();
     }
 
-    private void practiceOf() {
+    private void practiceOptionalOf() {
         String s1 = "foo";
         try {
             Optional.of(s1);
@@ -20,4 +21,21 @@ public class OptionalSample {
             System.out.println("Optional.of(null) throw NullPointerException");
         }
     }
+
+    private void practiceAbsent() {
+        Optional ret = Optional.absent();
+        if (java.util.Objects.isNull(ret)) {
+            System.out.println("ret is null");
+        }
+        if (ret.isPresent()) {
+            System.out.println("ret is not null");
+        } else {
+            System.out.println("ret is absent");
+        }
+    }
+
+    private void fromNullable() {
+
+    }
+
 }
