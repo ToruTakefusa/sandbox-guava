d09cd2a0ffeb9ccd3aa44224eb16b052fe3e1d72
Toru Takefusa <torutakefusa@gmail.com>
2020-08-15 11:47:09 +0900

    Add Optional.fromNullable(), Optional.isPresent(), Optional.get()

--- src/main/java/OptionalSample.java
+++ src/main/java/OptionalSample.java
@@ -11,4 +11,40 @@ public class OptionalSample {
     public boolean isOptionalAbsentPresent() {
         return Optional.absent().isPresent();
     }
+
+    public boolean isFromNullableNull() {
+        return java.util.Objects.isNull(Optional.fromNullable(null));
+    }
+
+    public boolean isFromNullableNullPresent() {
+        return Optional.fromNullable(null).isPresent();
+    }
+
+    public boolean isFromNullableStringPresent() {
+        return Optional.fromNullable("foo").isPresent();
+    }
+
+    public String getString() {
+        return Optional.of("foo").get();
+    }
+
+    public String getEmptyString() {
+        return Optional.of("").get();
+    }
+
+    public int getInt() {
+        return Optional.of(1).get();
+    }
+
+    public double getDouble() {
+        return Optional.of(3.14).get();
+    }
+
+    public Object getNull() {
+        return Optional.fromNullable(null).get();
+    }
+
+    public Object getAbsent() {
+        return Optional.absent().get();
+    }
 }
--- src/test/java/OptionalSampleTest.java
+++ src/test/java/OptionalSampleTest.java
@@ -27,4 +27,49 @@ class OptionalSampleTest {
     void isOptionalAbsentPresent() {
         assertFalse(optionalSample.isOptionalAbsentPresent());
     }
+
+    @Test
+    void isFromNullableNull() {
+        assertFalse(optionalSample.isFromNullableNull());
+    }
+
+    @Test
+    void isFromNullableNullPresent() {
+        assertFalse(optionalSample.isFromNullableNullPresent());
+    }
+
+    @Test
+    void isFromNullableStringPresent() {
+        assertTrue(optionalSample.isFromNullableStringPresent());
+    }
+
+    @Test
+    void getString() {
+        assertEquals(optionalSample.getString(), "foo");
+    }
+
+    @Test
+    void getEmptyString() {
+        assertEquals(optionalSample.getEmptyString(), "");
+    }
+
+    @Test
+    void getInt() {
+        assertEquals(optionalSample.getInt(), 1);
+    }
+
+    @Test
+    void getDouble() {
+        assertEquals(optionalSample.getDouble(), 3.14, 0.0001);
+    }
+
+    @Test
+    void getNull() {
+        assertThrows(IllegalStateException.class, ()->optionalSample.getNull());
+    }
+
+    @Test
+    void getAbsent() {
+        assertThrows(IllegalStateException.class, ()->optionalSample.getAbsent());
+    }
 }
